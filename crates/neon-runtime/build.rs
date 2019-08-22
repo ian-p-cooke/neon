4802e85e26908f2a684a7cde16e686e88c43cc07
Ian P. Cooke <ipc@informatic.io>
2019-08-22 04:02:04 -0500

    bah

--- crates/neon-runtime/build.rs
+++ crates/neon-runtime/build.rs
@@ -126,7 +126,7 @@ fn link_library() {
     let mut cmd = cc::Build::new().object(object_path).object(hook_object_path).get_compiler().to_command();
     let out = env::var("OUT_DIR").unwrap();
     let out = Path::new(&out);
-    let out = String::from("-out:") + out.join("libneon.a").into();
+    let out = String::from("-out:") + &String::from(out.join("libneon.a"));
     cmd.arg(&out);
     cmd.arg("/DELAYLOAD:node.exe");
     let status = cmd.status();
