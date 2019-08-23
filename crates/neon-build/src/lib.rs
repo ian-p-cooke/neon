6149f5413a24dc11a9f5b7e3a93e6415a899d34f
Ian P. Cooke <icooke@3redpartners.com>
2019-08-23 04:26:01 -0500

    "ship" the delay hook so that it can be built by dependents instead of by neon-runtime

--- crates/neon-build/src/lib.rs
+++ crates/neon-build/src/lib.rs
@@ -1,5 +1,9 @@
+extern crate cc;
+
 use std::env;
 use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
 /// Set up the build environment by setting Cargo configuration variables.
 pub fn setup() {
@@ -15,7 +19,20 @@ pub fn setup() {
         println!("cargo:rustc-link-search={}\\{}", node_root_dir, configuration);
         println!("cargo:rustc-link-search=native={}", &node_lib_path.display());
         println!("cargo:rustc-link-lib={}", &node_lib_file_path.file_stem().unwrap().to_str().unwrap());
+
+        //for win_delay_load_hook.cc
+        let win_delay_load_hook_cc = include_str!("win_delay_load_hook.cc");
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let out_dir_path = Path::new(&out_dir);
+        let path = out_dir_path.join("win_delay_load_hook.cc");
+        {
+            let mut file = File::create(&path).unwrap();
+            file.write_all(win_delay_load_hook_cc.as_bytes()).unwrap();
+        }        
+        cc::Build::new().define("HOST_BINARY", "\"node.exe\"").file(&path).compile("win_delay_load_hook.obj");
+        println!("cargo:rustc-link-lib=win_delay_load_hook.obj");
+        
         println!("cargo:rustc-cdylib-link-arg=/DELAYLOAD:node.exe");
-        println!("cargo:rustc-link-lib=delayimp");
+        println!("cargo:rustc-link-lib=delayimp");        
     }
 }
